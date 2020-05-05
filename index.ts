50cbae52afde6439bddb1173f6b5e6894b658412
g-homebase <gabidi@gmail.com>
2020-05-05 15:43:10 -0400

    Add sifir client to dev

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ export { client as btcClient } from "./src/clients/btcClient";
 export { client as lnClient } from "./src/clients/lncClient";
 export { client as otsClient } from "./src/clients/otsClient";
 export { client as wasabiClient } from "./src/clients/wasabiClient";
+export { client as sifirClient } from "./src/clients/sifirClient"
 export {
   default as cypherNodeHttpTransport
 } from "./src/transport/cypherNodeHttpTransport";
---
+++ src/clients/sifirClient.ts
@@ -0,0 +1,43 @@
+import { RestfulTransport } from "../lib/types/clients";
+import {
+  ConnectionString,
+  LnNodeInfo,
+  CreatedInvoice,
+  CreateInvoicePayload,
+  LnAddress,
+  Bolt11String,
+  DecodedBolt11,
+  CypherNodeLncClient,
+  LnConnectAndFundPayload,
+  LnConnectAndFundResult,
+  LnListPeersPayload,
+  LnPayBolt11Payload,
+  LnRouteDetails,
+  LnListFundsPayload,
+  LnListPaysPayload
+} from "../lib/types/lightning-c";
+export interface SifirGetLnWalletSnapshotPayload {
+  funds: LnListFundsPayload;
+  invoices: CreatedInvoice;
+  pays: LnListPaysPayload;
+}
+export interface SifirClient {
+  getBtcWalletSnapshot(param): Promise<LnNodeInfo>;
+  getLnWalletSnapshot(param): Promise<SifirGetLnWalletSnapshotPayload>;
+}
+export const client = ({
+  transport
+}: {
+  transport: RestfulTransport;
+}): SifirClient => {
+  const { get, post } = transport;
+  const api = {
+    getBtcWalletSnapshot(param): Promise<LnNodeInfo> {
+      return get("sifirGetBtcWalletSnapshot", param);
+    },
+    getLnWalletSnapshot(param): Promise<SifirGetLnWalletSnapshotPayload> {
+      return get("sifirGetLnWalletSnapshot", param);
+    }
+  };
+  return api;
+};
