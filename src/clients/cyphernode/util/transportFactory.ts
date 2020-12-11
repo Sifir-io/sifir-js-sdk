a2f7f11dbaeac29aac19079b386b3ae28b43a8f9
g-homebase <gabidi@gmail.com>
2020-12-11 10:22:54 -0500

    fix: [transport factories]  injectable param

--- src/clients/cyphernode/util/transportFactory.ts
+++ src/clients/cyphernode/util/transportFactory.ts
@@ -1,11 +1,19 @@
 import transport from "../../../transport/httpTransport";
-import {cypherNodeAuthHelper} from "./auth";
-const {customHeaders} = cypherNodeAuthHelper();
-
-const CypherNodeGatewayUrl =
-    (process && process.env.CYPHER_GATEWAY_URL) || "https://localhost:2009/v0/";
-const CypherNodeCertCAPem =
-    (process && process.env.CYPHERNODE_GATEKEEPER_CERT_CA) || "";
-const TorProxyURL = process.env.TOR_PROXY_URL || "";
-
-export default ()=>transport({gatewayUrl: CypherNodeGatewayUrl,customHeaders,caCert:CypherNodeCertCAPem,proxyUrl:TorProxyURL});
+import { cypherNodeAuthHelper as _authHelper } from "./auth";
+export default ({
+  CypherNodeGatewayUrl = (process && process.env.CYPHER_GATEWAY_URL) ||
+    "https://localhost:2009/v0/",
+  CypherNodeCertCAPem = (process &&
+    process.env.CYPHERNODE_GATEKEEPER_CERT_CA) ||
+    "",
+  TorProxyURL = process.env.TOR_PROXY_URL || "",
+  cypherNodeAuthHelper = _authHelper()
+} = {}) => {
+  const { customHeaders } = cypherNodeAuthHelper;
+  return transport({
+    gatewayUrl: CypherNodeGatewayUrl,
+    customHeaders,
+    caCert: CypherNodeCertCAPem,
+    proxyUrl: TorProxyURL
+  });
+};
