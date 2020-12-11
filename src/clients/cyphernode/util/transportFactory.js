be4a2392b4bc4fd782ca171cc097a6766fa64739
g-homebase <gabidi@gmail.com>
2020-12-11 10:23:08 -0500

    fix: [transport factories]  injectable param

--- src/clients/cyphernode/util/transportFactory.js
+++ src/clients/cyphernode/util/transportFactory.js
@@ -5,8 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const httpTransport_1 = __importDefault(require("../../../transport/httpTransport"));
 const auth_1 = require("./auth");
-const { customHeaders } = auth_1.cypherNodeAuthHelper();
-const CypherNodeGatewayUrl = (process && process.env.CYPHER_GATEWAY_URL) || "https://localhost:2009/v0/";
-const CypherNodeCertCAPem = (process && process.env.CYPHERNODE_GATEKEEPER_CERT_CA) || "";
-const TorProxyURL = process.env.TOR_PROXY_URL || "";
-exports.default = () => httpTransport_1.default({ gatewayUrl: CypherNodeGatewayUrl, customHeaders, caCert: CypherNodeCertCAPem, proxyUrl: TorProxyURL });
+exports.default = ({ CypherNodeGatewayUrl = (process && process.env.CYPHER_GATEWAY_URL) ||
+    "https://localhost:2009/v0/", CypherNodeCertCAPem = (process &&
+    process.env.CYPHERNODE_GATEKEEPER_CERT_CA) ||
+    "", TorProxyURL = process.env.TOR_PROXY_URL || "", cypherNodeAuthHelper = auth_1.cypherNodeAuthHelper() } = {}) => {
+    const { customHeaders } = cypherNodeAuthHelper;
+    return httpTransport_1.default({
+        gatewayUrl: CypherNodeGatewayUrl,
+        customHeaders,
+        caCert: CypherNodeCertCAPem,
+        proxyUrl: TorProxyURL
+    });
+};
--- src/clients/lnd/util/transportFactory.js
+++ src/clients/lnd/util/transportFactory.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const httpTransport_1 = __importDefault(require("../../../transport/httpTransport"));
 const auth_1 = require("./auth");
-const { customHeaders } = auth_1.lndHelper();
-const gatewayUrl = (process && process.env.LND_GATEWAY_URL) || "https://localhost:2009/v0/";
-const caCert = (process && process.env.LND_CA_CERT) || "";
-const proxyUrl = process.env.TOR_PROXY_URL || "";
-exports.default = () => httpTransport_1.default({ gatewayUrl, customHeaders, caCert, proxyUrl });
+exports.default = ({ gatewayUrl = (process && process.env.LND_GATEWAY_URL) ||
+    "https://localhost:3007/", caCert = (process && process.env.LND_CA_CERT) || "", proxyUrl = process.env.TOR_PROXY_URL || "", lndHelper = auth_1.lndHelper() } = {}) => {
+    const { customHeaders } = lndHelper;
+    return httpTransport_1.default({ gatewayUrl, customHeaders, caCert, proxyUrl });
+};
